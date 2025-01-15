20f51be3c27fa19478c0118deb125da5064bf756
Olabanj <olabanjolukman5@gmail.com>
2025-01-15 20:39:15 +0100

    media query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,8 @@
 import { Navbar, TextInput } from "flowbite-react";
 import { BiChevronDown } from "react-icons/bi";
 import { IoSearchOutline } from "react-icons/io5";
-import mybag from '../assets/images/shopping-bag.png';
-import use from '../assets/images/user.png';
-
+import mybag from "../assets/images/shopping-bag.png";
+import use from "../assets/images/user.png";
 
 export default function AppNavbar() {
   return (
@@ -12,40 +11,40 @@ export default function AppNavbar() {
         <div className="text-customBlue text-xl font-Mukta font-bold flex-1">
           <h2>Cosmo</h2>
         </div>
-        <Navbar.Toggle className="sm:block"/>
-      <Navbar.Collapse>
-
-        <div className="flex-1">
-          <ul className="list-none flex  gap-6 font-mukta text-[20px] text-customGray font-light">
-            <li>Sales</li>
-            <li>Hoodie</li>
-            <li>Fashion</li>
-            <li className="flex items-center">More <BiChevronDown /> <span className="ml-2"></span></li>
-          </ul>
-        </div>
-
-        <div className="flex gap-3 ">
-        <form className="flex items-center border border-gray-300 rounded-2xl px-1 py-0.5 h-[25px] w-[183px]">
-  <IoSearchOutline className="h-4 w-4 text-gray-500 inline" />
-  <span className="mr-1"></span>
-  <input
-    type="text"
-    placeholder="Search..."
-    className="border-none focus:ring-0  flex-1 text-xs text-customGray font-mukta font-light px-0"
-  />
-</form>
-
-
-
-          <article className="flex items-center border border-gray-300 rounded-lg h-[25px] w-[40] px-2 py-1">
-            <img src={mybag} alt="shopping bag " />
-            <span className="ml-1 font-mukta font-light text-sm text-customGray">0</span>
-          </article>
-          <div>
-            <img src={use} alt="user icon" className="w-[30] h-[25]"/>
+        <Navbar.Toggle className="block sm:hidden" />
+        <Navbar.Collapse>
+          <div className="flex-1 hidden sm:flex">
+            <ul className="list-none flex  gap-4 md:gap-6 font-mukta text-[24px] md:text-[20px] text-customGray font-light">
+              <li>Sales</li>
+              <li>Hoodie</li>
+              <li>Fashion</li>
+              <li className="flex items-center">
+                More <BiChevronDown /> <span className="ml-2"></span>
+              </li>
+            </ul>
           </div>
-          
-        </div>
+
+          <div className="flex gap-3 ">
+            <form className="flex items-center border border-gray-300 rounded-2xl px-1 py-0.5 h-[25px] w-[183px]">
+              <IoSearchOutline className="h-4 w-4 text-gray-500 inline" />
+              <span className="mr-1"></span>
+              <input 
+                type="text"
+                placeholder="Search..."
+                className="border-none focus:ring-0  flex-1 text-xs text-customGray font-mukta font-light px-0 bg-transparent "
+              />
+            </form>
+
+            <article className="flex items-center border border-gray-300 rounded-lg h-[25px] w-[40] px-2 py-1">
+              <img src={mybag} alt="shopping bag " />
+              <span className="ml-1 font-mukta font-light text-sm text-customGray">
+                0
+              </span>
+            </article>
+            <div>
+              <img src={use} alt="user icon" className="w-[30] h-[25]" />
+            </div>
+          </div>
         </Navbar.Collapse>
       </nav>
     </Navbar>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,12 +12,12 @@ import { MdStarBorder } from "react-icons/md";
 export default function AppProducts() {
   return (
     <main className="w-full">
-      <div className="w-[90%]  px-11  justify-center flex flex-column gap-10 mx-10 md:flex-row 
+      <div className="w-[90%]  px-11  justify-center flex flex-col gap-10 mx-10 md:flex-row  md:px-11
       ">
         
-        <section className=''>
+        <section className='md:w-1/2'>
           
-          <ul className="flex list-none gap-3 text-sm font-mukta font-light py-3 sm:text-l">
+          <ul className="flex list-none gap-3 text-sm font-mukta font-light py-3 sm:text-base">
             <li>Home</li>
             <li className="flex items-center">
               <MdKeyboardArrowRight />
@@ -33,40 +33,40 @@ export default function AppProducts() {
           <img
             src={hood}
             alt="Hoodie"
-            className="border rounded-2xl w-[375px] h-[330px] object-cover"
+            className="border rounded-2xl w-[375px] h-[330px] object-cover sm:w-[100%] "
           />
 
           
-          <div className="flex gap-6 mt-5">
+          <div className="flex gap-6 mt-5 sm:gap-5">
             <img
               src={small1}
               alt="Small Hoodie 1"
-              className="border rounded-2xl w-[80px] h-[80px] object-cover"
+              className="border rounded-2xl w-[80px] h-[80px] object-cover sm:w-[150px] sm:h-[150px] "
             />
             <img
               src={small2}
               alt="Small Hoodie 2"
-              className="border rounded-2xl w-[80px] h-[80px] object-cover"
+              className="border rounded-2xl w-[80px] h-[80px] object-cover sm:w-[150px] sm:h-[150px]"
             />
             <img
               src={small3}
               alt="Small Hoodie 3"
-              className="border rounded-2xl w-[80px] h-[80px] object-cover"
+              className="border rounded-2xl w-[80px] h-[80px] object-cover sm:w-[150px] sm:h-[150px]"
             />
           </div>
         </section>
 
         
-        <section className="h-[300px]">
+        <section className="h-[300px] md:w-1/2">
         
-          <h4 className="font-roboto font-semibold text-[32px] text-[#2D2D2D]  w-[55%]  pt-8">
+          <h4 className="font-roboto font-semibold text-[24px] text-[#2D2D2D]  w-[55%]  pt-8 md:text-[32px] sm:w-[75%]">
             Plaid Sleeve Kangaroo Pocket Drawstring Hoodie
           </h4>
 
           
-          <p className="font-roboto font-bold text-[16px] mt-3 ">
+          <p className="font-roboto font-bold text-[16px] mt-3 md:text-[14px]">
             BY
-            <span className="text-customBlue text-[16px] ml-1">COSMO FASHION</span>
+            <span className="text-customBlue text-[16px] ml-1 md:text-[14px]">COSMO FASHION</span>
           </p>
 
           
@@ -79,21 +79,21 @@ export default function AppProducts() {
 
             
             <div>
-              <p className="text-[#29B6F6] font-roboto text-[20px] font-semibold">
+              <p className="text-[#29B6F6] font-roboto text-[20px] font-semibold md:text-[18 px]">
                 Save 25%
               </p>
-              <p className="font-roboto text-[14px] text-customGray sm:text-lg">
+              <p className="font-roboto text-[20px] text-customGray md:text-[18px]">
                 Inclusive deal of the day
               </p>
             </div>
           </div>
 
-          <p className="font-roboto text-[16px] font-light w-[65%] mt-4">
+          <p className="font-roboto text-[20px] font-light w-[85%] mt-4 md:text-[16px] sm:w-[65%]">
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
           </p>
 
         
-          <div className="flex  gap-5 mt-4">
+          <div className="flex flex-col sm:flex-row gap-5 mt-4">
             <div className="border rounded-2xl border-gray-300 h-[45px] w-[90px] font-roboto font-semibold text-[14px] text-[#7D7D7D] px-3 py-2 flex items-center justify-between">
               QTY 2
               <BiChevronDown />
@@ -108,21 +108,21 @@ export default function AppProducts() {
             </div>
           </div>
           <hr className="mt-3"/>
-          <div className="flex pt-6 gap-3">
+          <div className="flex pt-3 gap-1 md:gap-2 md:pt-6">
             <section>
-            <img src={use} />
+            <img src={use} className="w-[20px] h-[20px] md:w-[16px] md:h-[10px] mt-2"/>
             </section>
-            <section>
-                <h3 className="font-roboto text-[16px] font-semibold text-customGrey">Alex Morningstar</h3>
-                <h4 className="font-roboto text-[14px] font-semibold text-customGrey">Contrary to popular belief</h4>
-                <p className="font-roboto text-[12px] font-light text-customGrey w-[60%]">lorem ipsum is not simply random text. it has root in a piece of classical Latin</p>
+            <section className="flex-1">
+                <h3 className="font-roboto text-[20px] font-semibold text-customGrey md:text-[16px]">Alex Morningstar</h3>
+                <h4 className="font-roboto text-[18px] font-semibold text-customGrey md:text-[14px]">Contrary to popular belief</h4>
+                <p className="font-roboto text-[18px] font-light text-customGrey w-[100%] md:text-[14px] md:w-[80%] lg:w-[75%]">lorem ipsum is not simply random text. it has root in a piece of classical Latin</p>
             </section>
             <section className="flex">
-            <IoStar className="text-[#1565C0]  h-[25px] w-[30px]" />
-            <IoStar className="text-[#1565C0] h-[25px] w-[30px]"/>
-            <IoStar className="text-[#1565C0] h-[25px] w-[30px]"/>
-            <IoStar className="text-[#1565C0] h-[25px] w-[30px]"/>
-            <MdStarBorder className="text-[#1565C0] h-[29px] w-[35px] border-blue-300"/>
+            <IoStar className="text-[#1565C0]  h-[28px] w-[33px] md:w-[30px] md:h-[25px]" />
+            <IoStar className="text-[#1565C0] h-[28px] w-[33px] md:w-[30px] md:h-[25px]"/>
+            <IoStar className="text-[#1565C0] h-[28px] w-[33px] md:w-[30px] md:h-[25px]"/>
+            <IoStar className="text-[#1565C0] h-[28px] w-[33px] md:w-[30px] md:h-[25px]"/>
+            <MdStarBorder className="text-[#1565C0] h-[30px] w-[35px] border-blue-300 md:w-[35px] md:h-[][29px]"/>
            
             </section>
 
